07e3f8c865ab0d569f276b024f5b6dd7d12afeff
CharanKondaveeti <sivacharankondaveeti@gmail.com>
2023-10-25 22:01:03 +0530

    3rd change"

--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable react/prop-types */
 function Header({ children }) {
-  const headerStyles = "px-0 flex flex-col h-[85vh] bg-custom-yellow relative ";
+  const headerStyles =
+    "px-0 flex flex-col h-[70vh] md:h-[85vh] bg-custom-yellow relative ";
   return <div className={headerStyles}>{children}</div>;
 }
 
--- src/header/Showcase.jsx
+++ src/header/Showcase.jsx
@@ -1,21 +1,21 @@
 // prettier-ignore
 function Showcase() {
   const parentStyles =
-    "py-2 flex-1 px-10 grid grid-cols-2 grid-rows-1 items-center gap-10 md:py-5   ";
-  const h1Styles = "text-7xl mb-10 font-gtMedium md:text-8xl  md:mb-10";
+    "py-2 flex-1 px-10 grid grid-cols-1 md:grid-cols-2 grid-rows-1 items-center gap-10 md:py-5   ";
+  const h1Styles = "text-7xl mb-6 font-gtMedium md:text-8xl  md:mb-10";
 
   return (
     <div className={parentStyles}>
-      <div className="pl-5 md:pl-10 flex flex-col items-start	">
+      <div className="pl-0 md:pl-5 md:pl-10 flex flex-col items-start	">
         <h1 className={h1Styles}>Stay curious</h1>
 
-        <p className="text-1xl mb-10 md:text-2xl md:mb-10">
+        <p className="text-1xl font-semibold md:font-normal mb-12 md:text-2xl md:mb-10">
           {"Discover stories, thinking, and expertise from writers on any topic."
             .split("<br/>")
             .join("\n")}
         </p>
 
-        <button className="text-[#f5f5f5] text-xl font-poppins px-16 py-2 bg-stone-950 rounded-full">
+        <button className="text-[#f5f5f5] text-sm md:text-xl font-poppins px-12 md:px-16 py-2 bg-stone-950 rounded-full">
           Start reading
         </button>
       </div>

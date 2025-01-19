445e38832140fe32f0b1bd98b0e908abcd1aff6a
CharanKondaveeti <sivacharankondaveeti@gmail.com>
2025-01-19 20:15:00 +0530

    devikarthik

--- src/header/Logo.jsx
+++ src/header/Logo.jsx
@@ -1,5 +1,5 @@
 function Logo() {
-  return <p className="text-3xl tracking-wider font-extrabold">BETWEEN</p>;
+  return <p className="text-3xl tracking-wider font-extrabold">DeviKarthik</p>;
 }
 
 export default Logo;

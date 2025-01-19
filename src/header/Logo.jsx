493d12590bd2478011ed12232b9eb0037b02064c
CharanKondaveeti <sivacharankondaveeti@gmail.com>
2025-01-19 20:16:18 +0530

    added kondaveeti

--- src/header/Logo.jsx
+++ src/header/Logo.jsx
@@ -1,5 +1,5 @@
 function Logo() {
-  return <p className="text-3xl tracking-wider font-extrabold">CHARAN</p>;
+  return <p className="text-3xl tracking-wider font-extrabold">CHARAN KOndaveeti</p>;
 }
 
 export default Logo;

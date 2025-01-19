1fbc5d07d39222f06a7b3b9bfa6f15880ed56a79
CharanKondaveeti <sivacharankondaveeti@gmail.com>
2025-01-19 20:14:14 +0530

    charan

--- src/header/Logo.jsx
+++ src/header/Logo.jsx
@@ -1,5 +1,5 @@
 function Logo() {
-  return <p className="text-3xl tracking-wider font-extrabold">BETWEEN</p>;
+  return <p className="text-3xl tracking-wider font-extrabold">CHARAN</p>;
 }
 
 export default Logo;
